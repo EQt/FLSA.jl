97d72c188b909751c868a60fb02fbad94102c717
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-11 17:46:45 +0100

    wip: started grid_graph method

--- src/grid_graph.jl
+++ src/grid_graph.jl
@@ -10,3 +10,14 @@ function grid_edges(n1::Integer, n2::Integer)
     Ev = [((i,j), (i,j+1)) for j in 1:n2-1, i in 1:n1]
     E = vcat(reshape(Eh, length(Eh)), reshape(Ev, length(Ev)))
 end
+
+typealias GridNode (Int, Int)
+typealias GridEdge Graphs.Edge{GridNode}
+typealias GridGraph IncidenceList{GridNode, GridEdge}
+
+"""Construct an incidence list representation of the n1×n2 grid graph"""
+function grid_graph(n1::Int, n2::Int)
+    V = grid_nodes(n1, n2)
+    E = grid_edges(n1, n2)
+    E = [GridEdge(i, u, v) for (i, (u, v)) in enumerate(E)]
+end
