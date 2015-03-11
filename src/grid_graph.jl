06412f9331c7e473df35be22f755dbde0c726587
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-11 17:56:18 +0100

    grid_graph works

--- src/grid_graph.jl
+++ src/grid_graph.jl
@@ -1,3 +1,8 @@
+typealias GridNode (Int, Int)
+typealias GridEdge Graphs.Edge{GridNode}
+typealias GridGraph EdgeList{GridNode, GridEdge}
+
+
 """Vector of nodes of an n1×n2 grid graph"""
 function grid_nodes(n1::Integer, n2::Integer)
     V = [(i,j) for j in 1:n2, i in 1:n1]
@@ -11,13 +16,10 @@ function grid_edges(n1::Integer, n2::Integer)
     E = vcat(reshape(Eh, length(Eh)), reshape(Ev, length(Ev)))
 end
 
-typealias GridNode (Int, Int)
-typealias GridEdge Graphs.Edge{GridNode}
-typealias GridGraph IncidenceList{GridNode, GridEdge}
-
 """Construct an incidence list representation of the n1×n2 grid graph"""
 function grid_graph(n1::Int, n2::Int)
     V = grid_nodes(n1, n2)
     E = grid_edges(n1, n2)
     E = [GridEdge(i, u, v) for (i, (u, v)) in enumerate(E)]
+    edgelist(V, E; is_directed=false)
 end
