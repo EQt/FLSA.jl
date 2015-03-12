1a7db8fc91f9d7aeaf4ee0732718415e375b1b87
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-12 17:24:01 +0100

    corrected minor mistake

--- test/incidence_matrix.jl
+++ test/incidence_matrix.jl
@@ -9,3 +9,16 @@ m = num_edges(G)
 @test ndims(D) == 2
 @test size(D, 1) == m
 @test size(D, 2) == n
+
+D23 = [# vertices
+       # 1  2  3  4  5  6
+        +1 -1  0  0  0  0; # edge 1
+         0 +1 -1  0  0  0; # edge 2
+         0  0  0 +1 -1  0; # edge 3
+         0  0  0  0 +1 -1; # edge 4
+        +1  0  0 -1  0  0; # edge 5
+         0 +1  0  0 -1  0; # edge 6
+         0  0 +1  0  0 -1; # edge 7
+       ]
+
+@test full(D) == D23
