179207bfbe14aafbaeffcebeae7548f09a0afe91
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-12 16:51:49 +0100

    introduce new function num_columns for grid graphs

--- src/grid_graph.jl
+++ src/grid_graph.jl
@@ -2,9 +2,11 @@ typealias GridNode (Int, Int)
 typealias GridEdge Graphs.Edge{GridNode}
 typealias GridGraph EdgeList{GridNode, GridEdge}
 
+"""Number of columns of the grid"""
+num_columns(g::GridGraph) = max(findfirst(x -> x == (2,1), g.vertices)-1, 1)
+
 """Specialized method for GridGraphs"""
-vertex_index(v::GridNode, g::GridGraph) =
-    max(findfirst(x -> x == (2,1), g.vertices)-1, 1)* (v[1]-1) + v[2]
+vertex_index(v::GridNode, g::GridGraph) = num_columns(g) * (v[1]-1) + v[2]
 
 """Vector of nodes of an n1×n2 grid graph"""
 function grid_nodes(n1::Integer, n2::Integer)
