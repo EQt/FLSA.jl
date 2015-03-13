d57f8c8ffedf89f2447dd1caed44f50ec4de8936
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-13 14:39:43 +0100

    introduced iteration counter k
    implemented update on x

--- src/admm.jl
+++ src/admm.jl
@@ -10,13 +10,26 @@ function admm{T<:Number,I<:Number}(y::Vector{T},
                                    D::AbstractMatrix{I},
                                    λ::Number = 1.0,
                                    δ::Number = 0.1,
-                                   μ::Number = 0.1)
+                                   μ::Number = 0.1;
+                                   max_iter::Int = 100,
+                                   verbose::Bool = false)
     m, n = size(D)
+    L = D'*D # Laplacian matrix
     x = copy(y) # initialize x
     b = zeros(n)
     v = zeros(m)
-    # TODO: update x
-    # TODO: update b
-    b = soft_threshold(D*x + v/μ, λ/μ, )
-    v += δ * (D*x - b)
+    k::Unsigned = 1 # iteration number
+    while k ≤ max_iter
+        k += 1
+        # update x
+        A = eye(L) + μ*L
+        c = y + D'*(μ*b - z)
+        
+        x = conjugate_gradient(A, c, c)
+        # update b
+        b = soft_threshold(D*x + v/μ, λ/μ, )
+        
+        # update v
+        v += δ * (D*x - b)
+    end
 end
