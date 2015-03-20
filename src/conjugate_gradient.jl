a2da3a461753a0639c1a052155fe476a0210fd0c
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-03-20 09:55:00 +0100

    lower bound

--- src/conjugate_gradient.jl
+++ src/conjugate_gradient.jl
@@ -12,7 +12,8 @@ function conjugate_gradient{T<:Number, R<:Real}(A::AbstractMatrix{T},
 
     r = A*x - b
     p = -r
-    ɛ *= norm(A, Inf) * norm(b, Inf)
+    println("ɛ=", ɛ)
+    ɛ = max(ɛ*norm(A, Inf) * norm(b, Inf), 1e-6)
     r_norm2 = norm2(r)
     k::Unsigned = 1
     while r_norm2 > ɛ && k ≤ max_iter
