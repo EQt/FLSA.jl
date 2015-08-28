9230de836178876daa50c4b24b1ac898035e9fc4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-08-29 00:12:22 +0200

    test find_x and that `+` does not effect the rest

--- test/pwl.jl
+++ test/pwl.jl
@@ -18,3 +18,15 @@ p4 = p3 + constid
 @test call(p4, -0.5) == -1.0
 @test call(p4, 2) == 2
 @test call(p4, 20) == 20
+
+# p3 should stay the same...
+@test call(p3, -5) == -5
+@test call(p3, -0.5) == -0.5
+@test call(p3, 2) == 0
+@test call(p3, 20) == 0
+
+
+@test find_x(p3,   -1) ==   -1
+@test find_x(p3, -0.5) == -0.5
+@test find_x(p4,   20) ==   20
+@test find_x(p4, -1.0) == -0.5
