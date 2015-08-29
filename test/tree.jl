7275ec4938c752c237b6df439e3723d674b3bc0b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-08-29 15:42:50 +0200

    DPVisitor

--- test/tree.jl
+++ test/tree.jl
@@ -17,3 +17,18 @@ end
 visitor = LogGraphVisitor(STDOUT)
 alg = DepthFirst()
 traverse_graph(gmst, alg, (1,1), visitor)
+
+type DPVisitor <: AbstractGraphVisitor
+    y::Vector{Float64}
+    lb::Vector{Float64}
+    ub::Vector{Float64}
+    df::Vector{PWL}
+    function DPVisitor(values::Vector{Float64})
+        n = length(values)
+        y = values
+        lb = fill(-Inf, n)
+        ub = fill(+Inf, n)
+        df = fill(PWL([]), n)
+    end
+end
+
