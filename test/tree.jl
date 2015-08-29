fb81960fe9babfe8405fa50e524a0c1b7d6aa72f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-08-29 15:48:44 +0200

    more definitions

--- test/tree.jl
+++ test/tree.jl
@@ -4,12 +4,14 @@ using Graphs
 
 n1, n2 = 4, 2
 g = grid_graph(n1, n2)
+v = vertices(g)
+n = length(v)
 w = rand(size(g.edges))
 
 mst, wmst = kruskal_minimum_spantree(g, w)
 # Problem: edge indices are relative to g, not gmst.
 # Because Edge is immutable: add all edges by myself
-gmst = graph(vertices(g), Edge{(Int64,Int64)}[]; is_directed=false)
+gmst = graph(v, Edge{(Int64,Int64)}[]; is_directed=false)
 for e in mst
     add_edge!(gmst, source(e), target(e))
 end
