9efdf86f870cddcc739cfb34a3e3a44b8c058d0a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-08-30 15:31:07 +0200

    outsourced code

--- src/tree.jl
+++ src/tree.jl
@@ -3,36 +3,48 @@
 using Graphs
 
 type TreeSubGraph{V,E}
-    g::AbstractGraph{V,E}
+    graph::AbstractGraph{V,E}
     edges::Vector{E}
     root::V
     parent::Vector{Int}
-    children::Vector{Vsdfasdfaector{Int}}
+    children::Vector{Vector{Int}}
     dfs_order::Vector{Int}
 end
 
 function subtree(graph, edges, root)
-    local n = num_vertices(g)
-    local neighbors = fill(Int[], n)
+    local n = num_vertices(graph)
     parent = fill(-1, n)
     children = fill(Int[], n)
     dfs_order = fill(-1, n)
-    for e in edges
-        is, it = vertex_index(source(e), graph), vertex_index(target(e), graph)
+    tree = TreeSubGraph(graph, edges, root, parent, children, dfs_order)
+    init_tree(tree)
+    tree
+end
+
+function init_tree(t::TreeSubGraph)
+    local n = num_vertices(t.graph)
+    local neighbors = fill(Int[], n)
+    for e in t.edges
+        is, it = vertex_index(source(e), t.graph), vertex_index(target(e), t.graph)
         push!(neighbors[is], it)
         push!(neighbors[it], is)
     end
     local dfs_num::Int = 1
-    local stack = [vertex_index(root, graph)]
+    local stack = [vertex_index(t.root, t.graph)]
     while !isempty(stack)
         v = pop!(stack)
-        dfs_order[dfs_num] = v
+        t.dfs_order[dfs_num] = v
         dfs_num += 1
-        children[v] = neighbors[v]
-        for u in children[v]
-            parent[u] = v
+        t.children[v] = neighbors[v]
+        for u in t.children[v]
+            t.parent[u] = v
+            iv = findfirst(neighbors[u], v)
+            println("iv = $iv, length(nei) = $(length(neighbors[u]))")
+            deleteat!(neighbors[u], iv)
             push!(stack, u)
         end
     end
-    TreeSubGraph(graph, edges, root, parent, children)
+    tree
 end
+
+                           
