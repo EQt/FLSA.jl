adfa921c7919684d89a2a035de282c37a09776ee
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-08-30 15:51:53 +0200

    define flsa instance

--- src/flsa.jl
+++ src/flsa.jl
@@ -3,3 +3,10 @@ norm2(x) = (x' * x)[1]
 
 """The objective function value"""
 flsa(x, y, D, λ) = 0.5 * norm2(y-x) + λ*norm(D*x, 1)
+
+"""Input to compute the graph induced fused LASSO signal approximator FLSA"""
+type Instance
+    y::Vector{Float64}
+    lambda::Float64
+    graph::AbstractGraph
+end
