1b6946a10b9c5ff3dbde3937cb21a8d1aeec2103
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-09-05 14:41:34 +0200

    use FactCheck unit testing library

--- test/treepwl.jl
+++ test/treepwl.jl
@@ -1,8 +1,17 @@
+using FactCheck
+
 children = Array{Int}[[], [], [], [1,2,3]]
 lb = [1, 2.5, 2, -Inf]
 ub = [3.2, 3, 4.5, Inf]
 y4 = 2.5
 anode = PWLNode(children[4], y4, 4, lb, ub)
 
-@assert (x = min_knot!(anode)) == 1.0 "x = $x"
-@assert (x = min_knot!(anode)) == 2.0 "x = $x"
+facts("a simple node (depth 1)") do
+    @fact min_knot!(anode) --> 1.0
+    @fact min_knot!(anode) --> 2.0
+    @fact min_knot!(anode) --> 2.5
+    @fact min_knot!(anode) --> 3.0
+    @fact min_knot!(anode) --> 3.2
+    @fact min_knot!(anode) --> 4.5
+    @fact min_knot!(anode) --> Inf
+end
