b93ca912b191af6c3b8be59f73318c93a346201d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-09-13 19:53:11 +0200

    be able to dynamically activate debugging

--- src/debug.jl
+++ src/debug.jl
@@ -1,10 +1,14 @@
 """Debugging functionality of FLSA"""
 
-if isdefined(:debug)
+p = module_parent(current_module())
+if isdefined(p, :debug) && typeof(p.debug) == Function
+    info("Activating DEBUGGING in FLSA")
     macro debug(msg)
-        :(info($msg))
+        :(p.debug($msg))
     end
 else
+    info("Deactivating debugging in FLSA")
     macro debug(msg)
+        # do nothing
     end
 end
