371cf5384f1d2f409826bda60b300c540aab03bc
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-09-13 20:06:13 +0200

    give the compiler more information

--- src/dynamic.jl
+++ src/dynamic.jl
@@ -31,10 +31,10 @@ function backtrace_dp_tree(vis, iroot, t, y)
 end
 
 """Compute x=FLSA(y, lambda) on a a (sub)tree t, naive PWL implementation"""
-function dp_tree_naive(y::Vector{Float64},
-                       lambda::Float64,
-                       t::TreeSubGraph,
-                       vis::DPVisitor = DPVisitor(y))
+function dp_tree_naive{V,E}(y::Vector{Float64},
+                            lambda::Float64,
+                            t::TreeSubGraph{V,E},
+                            vis::DPVisitor = DPVisitor(y))
     for i in 1:num_vertices(t.graph)
         vis.df[i] = PWL([Knot(-INF, -INF - vis.y[i]),
                          Knot(+INF, +INF - vis.y[i])])
