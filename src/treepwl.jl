ed454c81c650c3e4346b8683396f89155cbb95b1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-09-16 21:20:21 +0200

    wip

--- src/treepwl.jl
+++ src/treepwl.jl
@@ -156,7 +156,7 @@ function step_max_old(t, v)
         pq_u = t.nodes[u].pq
     end
     try
-        print_tree(t)
+        #print_tree(t)
         if isempty(pq_u)
             return
         end
@@ -327,14 +327,20 @@ function forward_dp_treepwl(t)
     t2 = deepcopy(t)
     for i in t.pre_order[end:-1:1]
         prepare_node(t2, i)
+        lower_event2!(t2, i)
+        info("TTTT 22222")
+        print_tree(t2)
+
         lower_event!(t, i)
-        lower_event2!(t, i)
+        info("TTTT 11111")
         print_tree(t)
-        print_tree(t2)
-        # print_min(t, i)
-        upper_event!(t, i)
+
         upper_event2!(t2, i)
-        print_tree(t)
+        info("TTTT 22222")
         print_tree(t2)
+
+        upper_event!(t, i)
+        info("TTTT 11111")
+        print_tree(t)
     end
 end
