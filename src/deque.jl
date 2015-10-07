facfa98a017c1b41ce311c974702e090fae658ee
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-10-07 13:37:48 +0200

    push functions

--- src/deque.jl
+++ src/deque.jl
@@ -3,3 +3,5 @@ using DataStructures.Deque
 
 pop_front!{T}(q::Deque{T}) = shift!(q)
 pop_back!{T}(q::Deque{T}) = pop!(q)
+push_front!{T}(q::Deque{T}, x::T) = unshift!(q, x)
+push_back!{T}(q::Deque{T}, x::T) = push!(q, x)
