47e1c2b2d252d9895a708862f1f225af458f181b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-10-07 22:14:48 +0200

    simplified notation

--- src/line.jl
+++ src/line.jl
@@ -3,11 +3,12 @@
 function dp_line_naive(y::Vector{Float64}, lambda::Float64)
     n = length(y)
     lb, ub, x = fill(Inf, n), fill(-Inf, n), zeros(n)
-    df = PWL(0.0, -y[1]; slope=1.0)
+    q(i) = PWL(0.0, -y[1]; slope=1.0)
+    df = q(1)
     for i = 2:n
         lb[i-1] = find_x(df, -lambda)
         ub[i-1] = find_x(df, +lambda)
-        df = PWL(0.0, -y[i]; slope=1.0) + clip(df, lb[i-1], ub[i-1])
+        df = q(i) + clip(df, lb[i-1], ub[i-1])
     end
 
     x[n] = find_x(df, 0)
