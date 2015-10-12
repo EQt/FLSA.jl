79897c6c1bbad4b73fb1cfeadfedb320a3699765
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-10-12 22:55:51 +0200

    dp_line_backtrace

--- src/line.jl
+++ src/line.jl
@@ -1,8 +1,10 @@
 """Line algorithms"""
 
+
 function dp_line_naive(y::Vector{Float64}, λ::Float64)
     n = length(y)
-    lb, ub, x = fill(Inf, n), fill(-Inf, n), zeros(n)
+    lb, ub = fill(Inf, n), fill(-Inf, n)
+
     q(i) = PWL(0.0, -y[i]; slope=1.0)
     df = q(1)
     for i = 2:n
@@ -11,12 +13,16 @@ function dp_line_naive(y::Vector{Float64}, λ::Float64)
         df = q(i) + clip(df, lb[i-1], ub[i-1])
     end
 
-    x[n] = find_x(df, 0)
+    xn = find_x(df, 0)
+    return db_line_backtrace(xn, lb, ub)
+end
+
+function dp_line_backtrace(xn, lb, ub)
+    n = length(lb)
+    x = zeros(n)
+    x[n] = xn
     for i = n:-1:2
         x[i-1] = clip(x[i], lb[i-1], ub[i-1])
     end
-
     return x
 end
-
-        
