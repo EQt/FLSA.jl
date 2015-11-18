ffe2b1ff79e424cec1ffeac1e51dcf08f1365d15
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-11-18 20:57:41 +0100

    outsourced old code

--- src/dynamic.jl
+++ src/dynamic.jl
@@ -1,27 +1,6 @@
 ## Naive implementation of the dynamic programming algorithm on trees.
 
 
-"""Contains all information for the calculation"""
-type DPVisitor <: AbstractGraphVisitor
-    y::Vector{Float64}
-    lb::Vector{Float64}
-    ub::Vector{Float64}
-    df::Vector{PWL}
-    function DPVisitor(y::Vector{Float64})
-        n = length(y)
-        lb = fill(-Inf, n)
-        ub = fill(+Inf, n)
-        df = [PWL([]) for i in y]
-        new(y, lb, ub, df)
-    end
-end
-
-"""For testing the graph traversal mechanism"""
-function Graphs.close_vertex!(vis::DPVisitor, v)
-    println("processing $v")
-end
-
-
 """After computing the bounds [lb, ub] for each node, compute solution x
    by clipping each edge, i.e. backtracing from root to children"""
 function backtrace_dp_tree(x_root, iroot, t, n, ub, lb)
