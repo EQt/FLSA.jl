cd915539e42a7b862fcd8928144017818310e711
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-11-20 13:20:04 +0100

    comment

--- src/deque.jl
+++ src/deque.jl
@@ -6,9 +6,11 @@ typealias DeQue Deque
 """More convinient way of denoting the functions"""
 pop_front!{T}(q::DeQue{T}) = shift!(q)
 
+"""More convinient way of denoting the functions"""
 pop_back!{T}(q::DeQue{T}) = pop!(q)
 
+"""More convinient way of denoting the functions"""
 push_front!{T}(q::DeQue{T}, x::T) = unshift!(q, x)
 
+"""More convinient way of denoting the functions"""
 push_back!{T}(q::DeQue{T}, x::T) = push!(q, x)
-
