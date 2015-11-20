d3f813a15fe55adcf8c25d8634ed30b0e7ed1e82
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-11-20 13:05:15 +0100

    simplify test

--- test/dynamic.jl
+++ test/dynamic.jl
@@ -1,38 +1,25 @@
-module TestTreePWL
+module TestDynamic
+
 debug(msg) = println("DEBUG: $msg")
+
 using Graphs
 using FactCheck
 using FLSA
 
-
 facts("A random example") do
-    begin
-        srand(3)
-        lambda = 1.0
-        n1, n2 = 5, 3
-        g = FLSA.igraph(FLSA.grid_graph(n1, n2))
-        v = FLSA.vertices(g)
-        n = length(v)
-        w = rand(size(g.edges))
-        y = round(10*rand(n1*n2), 1)
-        root = 1
+    srand(13)
+    lambda = 1.0
+    n1, n2 = 5, 3
+    g = FLSA.igraph(FLSA.grid_graph(n1, n2))
+    v = FLSA.vertices(g)
+    n = length(v)
+    w = rand(size(g.edges))
+    y = round(10*rand(n1*n2), 1)
+    root = 1
 
-        mst, wmst = kruskal_minimum_spantree(g, w)
-        t = FLSA.subtree(g, mst, root)
-        tn = FLSA.PWLTree(t, y)
-        vis = FLSA.DPVisitor(y)
-        x = FLSA.dp_tree_naive(y, lambda, t, vis)
-
-        tf = FLSA.PWLTree(tn.parent, root, y, i->lambda)
-        for (i,yi) in enumerate(y)
-            println("$i: $yi --> $(tf.parent[i]) [$(round(vis.lb[i], 6)), $(round(vis.ub[i], 6))]")
-        end
-        FLSA.forward_dp_treepwl(tf)
-        for i=1:n
-            @fact tf.nodes[i].lb --> roughly(round(vis.lb[i], 6)) "lb i = $i"
-            @fact tf.nodes[i].ub --> roughly(round(vis.ub[i], 6)) "ub i = $i"
-        end
-    end
+    mst, wmst = kruskal_minimum_spantree(g, w)
+    t = FLSA.subtree(g, mst, root)
+    x = FLSA.dp_tree_naive(y, lambda, t)
 end
 
-end
+end # module
