df5b1c543d3e3453ed8611093625b13e925d2be0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-12-01 21:34:53 +0100

    be more specific

--- src/flsa.jl
+++ src/flsa.jl
@@ -7,7 +7,8 @@ clip(x::Vector{Float64}, lo::Float64, hi::Float64) = Base.clamp(x, lo, hi)
 norm2(x::Vector{Float64}) = dot(x, x)
 
 """The objective function value"""
-flsa(x, y, D, λ=1.0) = 0.5 * norm2(y-x) + λ*norm(D*x, 1)
+flsa(x, y::Vector{Float64}, D::IncMat) =
+    0.5 * norm2(y-x) + norm(D*x, 1)
 
 
 """Input to compute the graph induced fused LASSO signal approximator FLSA"""
