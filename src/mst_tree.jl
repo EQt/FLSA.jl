23cdea61f918a590a51b7b5f8d56ee238497930f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-12-02 15:08:03 +0100

    no global prefix

--- src/mst_tree.jl
+++ src/mst_tree.jl
@@ -4,8 +4,7 @@ function max_gap_tree(y::Vector{Float64}, g::FLSA.ImgGraph; c0 = 0.0,
                       logger = Dict{UTF8String,Any}(),
                       random_tree=false)
     function logg(msg); end
-    global alpha = c0 * sign(g.D*y[:])
-    # global alpha = zeros(num_edges(g.graph))
+    alpha = c0 * sign(g.D*y[:])
     gap = FLSA.duality_gap(y[:], alpha, g)
     logg("duality gap: $gap")
     dual_obj = FLSA.norm2(y[:] - g.D'*alpha)
@@ -16,15 +15,15 @@ function max_gap_tree(y::Vector{Float64}, g::FLSA.ImgGraph; c0 = 0.0,
         else
             weights = - FLSA.gap_vec(y[:], alpha, g)
         end
-        logg("computed weights")
+        logg("weights")
         mst, wmst = kruskal_minimum_spantree(g.graph, weights)
-        global mst, wmst
-        logg("computed tree")
-        global t = FLSA.subtree(g.graph, mst, 1)
+
+        logg("tree")
+        t = FLSA.subtree(g.graph, mst, 1)
         logg("created subtree")
-        global z = y[:] - FLSA.non_tree(g.D, mst)'*alpha
+        z = y[:] - FLSA.non_tree(g.D, mst)'*alpha
         logg("non_tree")
-        global Lam = fill(Inf, length(y))
+        Lam = fill(Inf, length(y))
         for e in mst
             v, u = source(e), target(e)
             if t.parent[v] == u
@@ -34,13 +33,13 @@ function max_gap_tree(y::Vector{Float64}, g::FLSA.ImgGraph; c0 = 0.0,
             end
         end
         logg("lambda subtree")
-        global x = FLSA.dp_tree(z, Lam , t)
+        x = FLSA.dp_tree(z, Lam , t)
         logg("dp_tree")
-        global alpha_t = FLSA.dual_tree(z, x, t)
+        alpha_t = FLSA.dual_tree(z, x, t)
         @debug("gap(tree-part) = $(norm(z - FLSA.tree_part(g.D, mst)' * alpha_t - x))")
         logg("dual_tree: \n$(alpha_t[1:min(5, length(alpha_t))])")
-        global m = num_edges(g.graph)
-        global alpha_old = deepcopy(alpha)
+        m = num_edges(g.graph)
+        alpha_old = deepcopy(alpha)
         for (i,e) in enumerate(mst)
             alpha[e.index] = alpha_t[i] / g.lambda[e.index]
         end
