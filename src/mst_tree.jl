345798b403613f204deaf66b60bb607a9a9f4175
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-12-02 14:59:06 +0100

    first running version

--- src/mst_tree.jl
+++ src/mst_tree.jl
@@ -1,14 +1,14 @@
-function mst_tree(y::Vector{Float64}, g::ImgGraph; c0 = 0.0,
-                  max_iter=1,
-                  logger = Dict{UTF8String,Any}())
-    alpha = c0 * sign(grid.D*y[:])
-
-        global alpha = 0.1 * sign(grid.D*y[:])
+max_gap_tree(y::Matrix{Float64}, g::FLSA.ImgGraph; params...) = max_gap_tree(y[:], g; params...)
+function max_gap_tree(y::Vector{Float64}, g::FLSA.ImgGraph; c0 = 0.0,
+                      max_iter=1,
+                      logger = Dict{UTF8String,Any}())
+    global alpha = c0 * sign(grid.D*y[:])
     # global alpha = zeros(num_edges(grid.graph))
     gap = FLSA.duality_gap(y[:], alpha, grid)
     logg("duality gap: $gap")
     dual_obj = FLSA.norm2(y[:] - grid.D'*alpha)
     logg("duality obj: $dual_obj")
+    # imshow(y; title="noisy")
     for it = 1:max_iter
         global weights = - FLSA.gap_vec(y[:], alpha, grid)
         # global weights = randn(num_edges(grid.graph))
@@ -21,7 +21,7 @@ function mst_tree(y::Vector{Float64}, g::ImgGraph; c0 = 0.0,
         global z = y[:] - FLSA.non_tree(grid.D, mst)'*alpha
         logg("non_tree")
         # imshow(reshape(z, n1, n2))
-        global Lam = fill(lam, length(y))
+        global Lam = fill(Inf, length(y))
         for e in mst
             v, u = source(e), target(e)
             if t.parent[v] == u
@@ -37,7 +37,6 @@ function mst_tree(y::Vector{Float64}, g::ImgGraph; c0 = 0.0,
         warn(norm(z - FLSA.tree_part(grid.D, mst)' * alpha_t - x))
         logg("dual_tree: \n$(alpha_t[1:min(5, length(alpha_t))])")
         global m = num_edges(grid.graph)
-
         global alpha_old = deepcopy(alpha)
         for (i,e) in enumerate(mst)
             alpha[e.index] = alpha_t[i] / grid.lambda[e.index]
