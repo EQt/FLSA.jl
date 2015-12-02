4adf1c221a22aafa1ad61276417f71ebb29765e5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2015-12-02 18:28:50 +0100

    serious bug

--- src/inplace.jl
+++ src/inplace.jl
@@ -1,7 +1,7 @@
 macro inplace(ex)
     if ex.head == :+=
         # broadcast(+, $(ex.args[1]), $(ex.args[2]), $(ex.args[2]))
-        :(BLAS.axpy!(1.0, $(ex.args[1]), $(ex.args[2])))
+        :(BLAS.axpy!(1.0, $(ex.args[2]), $(ex.args[1])))
     else
         ex
     end
