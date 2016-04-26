a1f168929d1d53d6b8fe620e9b5fe1a844dd6f5a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-26 13:39:14 +0200

    simplified admm code

--- src/admm.jl
+++ src/admm.jl
@@ -3,6 +3,24 @@ include("conjugate_gradient.jl")
 """Componentwise soft threshold on x by width λ"""
 soft_threshold(x, λ) = sign(x) .* max(0, abs(x) - λ)
 
+
+macro log_admm()
+    return quote
+        if verbose
+            if !haskey(logger, "flsa")
+                logger["time"] = []
+                logger["flsa"] = []
+                logger["ɛ_CG"] = []
+            end
+            push!(logger["flsa"], flsa(x, y, D))
+            push!(logger["time"], time)
+            push!(logger["ɛ_CG"], ɛ_CG)
+            println(@sprintf("%4d %f %f", k, logger["flsa"][end], norm(x-y, 2)))
+        end
+    end
+end    
+
+
 """
 Solve the FLSA by the ALTERNATING DIRECTION METHOD OF MULTIPLIERS.
 """
@@ -16,44 +34,26 @@ function admm(y::Vector{Float64},
               verbose::Bool = false,
               logger = Dict{UTF8String, Any}(),
               max_time::Number = Inf)
-    m, n = size(D)
-    size(y,1) == n ||
-       error(@sprintf("y has wrong dimension %d (should be %d", size(y,1), n))
-
-    L = D'*D # Laplacian matrix
-    @assert size(L, 1) == n
-    @assert size(L, 2) == n
-    x = copy(y) # initialize x
+    m, n = size(D)              # Incidence matrix
+    L = D'*D                    # Laplacian matrix
+    @assert size(y,1) == n @sprintf("size(y)=%d (n=%d)", size(y,1), n)
+    @assert size(L) == (n, n)
+    x = copy(y)                 # initializcation
     b = zeros(m)
     z = zeros(m)
-    k= 1 # iteration number
-    total = 0
+    k = 1                       # iteration number
+    total = 0                   # total runtime
     tic()
     while k ≤ max_iter && total ≤ max_time
-        time = toq()
-        total += time
-        if verbose
-            if !haskey(logger, "flsa")
-                logger["time"] = []
-                logger["flsa"] = []
-                logger["ɛ_CG"] = []
-            end
-            push!(logger["flsa"], flsa(x, y, D))
-            push!(logger["time"], time)
-            push!(logger["ɛ_CG"], ɛ_CG)
-            println(@sprintf("%4d %f %f", k, logger["flsa"][end], norm(x-y, 2)))
-        end
+        total += toq()                          # stop cloc for logging
+        @log_admm
         tic()
-        # update x
-        A = eye(L) + μ*L
-        c = y + D'*(μ*b - z)
-        x = conjugate_gradient(A, c, x; ɛ=ɛ_CG)
-        ɛ_CG *= ɛ_c
-        # update b
-        b = soft_threshold(D*x + z/μ, 1/μ)
-        
-        # update z
-        z += δ * (D*x - b)
+        A = eye(L) + μ*L                        # lhs matrix
+        c = y + D'*(μ*b - z)                    # rhs
+        x = conjugate_gradient(A, c, x; ɛ=ɛ_CG) # update x
+        ɛ_CG *= ɛ_c                             # update accuracy
+        b = soft_threshold(D*x + z/μ, 1/μ)      # update b
+        z += δ * (D*x - b)                      # update z
         k += 1
     end
     x
