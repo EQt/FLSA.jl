e176ee55703dc8158c41131eb97660f794c88505
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-10-31 12:08:54 +0100

    use more BLAS routines

--- perf/conjugate_gradient.jl
+++ perf/conjugate_gradient.jl
@@ -1,5 +1,6 @@
 norm2(x::Vector{Float64}) = dot(x,x)
 
+
 square{MatT<:AbstractMatrix{Float64}}(A::MatT, p::Vector{Float64}) =
     (p' * A * p)[1]
 
@@ -17,12 +18,17 @@ function conjugate_gradient{MatT<:AbstractMatrix{Float64}}(A::MatT,
     ɛ = max(ɛ*norm(A, Inf) * norm(b, Inf), 1e-6)
     r_norm2 = norm2(r)
     k::Unsigned = 1
+    Ap = Vector{Float64}(length(r))
     while r_norm2 > ɛ && k ≤ max_iter
-        α = - dot(r, p) / square(A, p)
-        x .+= α .* p
-        r .+= α .* A * p
+        Ap .= A * p
+        α = - dot(r, p) / dot(p, Ap)
+        # x .+= α .* p
+        BLAS.axpy!(α, p, x)
+        # r .+= α .* Ap
+        BLAS.axpy!(α, Ap, r) # r .+= α .* Ap
         β = norm2(r)/ r_norm2
-        p .*=  β
+        # p .*=  β
+        BLAS.scal!(length(p), β, p, 1)
         BLAS.axpy!(-1, r, p)
         k += 1
         r_norm2 = norm2(r)
@@ -30,12 +36,21 @@ function conjugate_gradient{MatT<:AbstractMatrix{Float64}}(A::MatT,
     x
 end
 
-
 n = 100
+srand(42)
 A = Matrix(Symmetric(rand(n,n)))
 @assert issymmetric(A)
+@assert abs(det(A)) > 1e-5
 b = rand(n)
 x = zeros(b)
 
-conjugate_gradient(A, b, x)
 
+# import FLSA
+
+conjugate_gradient(A, b, x)
+Profile.clear_malloc_data()
+x .= 0
+@allocated conjugate_gradient(A, b, x; max_iter=1000)
+let n = norm(A*x - b)
+    @assert n < 1e-2 "n = $n"
+end
