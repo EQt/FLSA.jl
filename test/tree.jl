c1e9be6bcd76442c360bcba87a88b280ab32464b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2017-04-12 13:38:24 +0200

    update test functions

--- test/tree.jl
+++ test/tree.jl
@@ -1,26 +1,29 @@
-"""Some tests how to compute MST in julia"""
-using FLSA
-using Graph
+# Some tests how to compute MST in julia
 
-srand(42)
+module TestTreeFunctions
 
-n1, n2 = 4, 2
-g = grid_graph(n1, n2)
-v = vertices(g)
-n = length(v)
-w = rand(size(g.edges))
-y = rand(n1*n2)
+import FLSA
+import Graph
 
-mst, wmst = kruskal_minimum_spantree(g, w)
-# Problem: edge indices are relative to g, not gmst.
-# Because Edge is immutable: add all edges by myself
-gmst = graph(v, Edge{(Int64,Int64)}[]; is_directed=false)
-for e in mst
-    add_edge!(gmst, source(e), target(e))
+function test1()
+    srand(42)
+
+    n1, n2 = 4, 2
+    img = FLSA.img_graph(n1, n2)
+    g = img.graph
+    v = Graph.vertices(g)
+    n = length(v)
+    w = rand(size(g.edges))
+    y = rand(n1*n2)
+
+    mst, wmst = Graph.kruskal_minimum_spantree(g, w)
+    lambda = 0.1
+    tm = FLSA.subtree(g, mst, 1)
+    x = FLSA.dp_tree(y, lambda, tm)
+    alpha = FLSA.dual_tree(y, x, tm)
+    abs(alpha) .<= lambda + 1e-9
 end
 
-lambda = 0.1
-tm = FLSA.subtree(g, mst, (1,1))
-x = FLSA.dp_tree(y, lambda, tm)
-alpha = FLSA.dual_tree(y, x, tm)
-abs(alpha) .<= lambda + 1e-9
+end
+
+TestTreeFunctions.test1()
