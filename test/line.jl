34196deedf4bf339a47c9ded365746ec95b65c4f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-01-16 10:02:48 +0100

    test/line.jl: get rid of FactCheck

--- test/line.jl
+++ test/line.jl
@@ -1,33 +1,33 @@
 module TestLine
-using FactCheck
 using FLSA
+using Base.Test
 
-facts("3 line knot") do
-    y = [1.0; 2.0; 3.5]
-    x = FLSA.dp_line_naive(y, 1.0)
-    @fact x --> roughly([2.0; 2.0; 2.5])
+@testset "line" begin
+    @testset "3 line knot" begin
+        y = [1.0; 2.0; 3.5]
+        x = FLSA.dp_line_naive(y, 1.0)
+        @test x ≈ [2.0; 2.0; 2.5]
+    end
+
+    @testset "line: 3 knots, fast" begin
+        y = [1.0, 2.0, 3.5]
+        x = FLSA.dp_line(y, 1.0)
+        @test x ≈ [2.0; 2.0; 2.5]
+    end
+
+    @testset "line: 5 knots same" begin
+        y = [1.0; 4.0; 3.5; 3.8; 4.1]
+        z = FLSA.dp_line_naive(y, 1.0)
+        x = FLSA.dp_line(y, 1.0)
+        @test x ≈ z
+    end
+
+    @testset "line: 3 rand knots same" begin
+        y = rand(srand(13), 20)
+        z = FLSA.dp_line_naive(y, 0.3)
+        x = FLSA.dp_line(y, 0.3)
+        d = 3
+        @test round.(x,d) ≈ round.(z,d)
+    end
 end
-
-facts("line: 3 knots, fast") do
-    y = [1.0, 2.0, 3.5]
-    x = FLSA.dp_line(y, 1.0)
-    @fact x --> roughly([2.0; 2.0; 2.5])
-end
-
-facts("line: 5 knots same") do
-    y = [1.0; 4.0; 3.5; 3.8; 4.1]
-    z = FLSA.dp_line_naive(y, 1.0)
-    x = FLSA.dp_line(y, 1.0)
-    @fact x --> roughly(z)
-end
-
-facts("line: 3 rand knots same") do
-    srand(13)
-    y = rand(20)
-    z = FLSA.dp_line_naive(y, 0.3)
-    x = FLSA.dp_line(y, 0.3)
-    d = 3
-    @fact round.(x,d) --> roughly(round.(z,d))
-end
-
 end
